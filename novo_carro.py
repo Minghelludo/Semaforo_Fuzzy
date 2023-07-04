c450abfe3f266cef4b5a1bc0eb3d7d22a09523d8
DenielR <97266017+DenielR@users.noreply.github.com>
2023-07-04 19:07:03 -0300

    Entrada aleatória de carros na fila

--- novo_carro.py
+++ novo_carro.py
@@ -1,3 +1,4 @@
+import random
 #entra um carro novo a cada 2 "segundos"
 def entrada_2_em_2(carros,tempo):
     if (tempo % 2) == 0:
@@ -10,4 +11,9 @@ def entrada_3_em_3(carros,tempo):
         carros.append(0)
     return carros
 
-#é possivel fazer uma entrada randomizada
+#entra um carro num intervalo aleatório entre 1 e 10 segundos
+def entrada_aleatoria(carros,tempo):
+    num_aleatorio = random.randint(1,10)
+    if (tempo % num_aleatorio) == 0:
+        carros.append(0)
+    return carros
